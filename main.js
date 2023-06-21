e04e580b4b93bd9c1028ea253da78277070edefb
efeld2015 <efeld@Emily-Felds-MacBook-Pro.local>
2023-06-20 21:28:37 -0500

    function declaration added

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
+//initialize + declare variables
 let selection = document.querySelector('select');
 let result = document.querySelector('h2');
+
+selection.addEventListener('change', (event) => {
+    result.innerText = selection.options[selection.selectedIndex].value;
+})
